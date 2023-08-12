091cc1f556ec258aa211bfe6374aa7ca9d2bb3de
Kimbyoungha <puze@naver.com>
2023-08-13 00:15:19 +0900

    스피릿 데이터화

---
+++ lib/routes/reward_route.dart
@@ -0,0 +1,11 @@
+import 'dart:async';
+
+import 'package:flame/components.dart';
+
+class RewardRoute extends Component {
+  @override
+  FutureOr<void> onLoad() {
+    // TODO: implement onLoad
+    return super.onLoad();
+  }
+}
